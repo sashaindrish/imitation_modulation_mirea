51a0b50c837311d10ce151cf586c3e44c85996a0
madskills700 <madskills700@gmail.com>
2021-11-25 22:06:48 +0300

    актуальные для 2 варианта коэффициенты

--- monte_carlo/solution/python/task1.py
+++ monte_carlo/solution/python/task1.py
@@ -24,15 +24,15 @@ def task():
 
     t = task_monte_carlo(6)
 
-    t.set_output_volume(6700, 450)
+    t.set_output_volume(5500, 270)
     t.set_tax(0.4)
-    t.set_residual_value(0.00007)
-    t.set_depreciation(2500)
+    t.set_residual_value(0.00005)
+    t.set_depreciation(2000)
     t.set_variable_costs(100, 160, 130)
-    t.set_discount_rate(14.5, 6.5)
+    t.set_discount_rate(8, 16)
     t.set_fixed_costs(40000, 60000)
     t.set_price_for_one(180, 230, 210)
-    t.set_start_up_investment(500000)
+    t.set_start_up_investment(400000)
 
     t.solution_model()
     t.print_cf_npv_pi()
