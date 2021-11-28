1a4e5df574c9962a90b5aae017debb1cb874dd4a
sashaindrish <alexrover2020@yandex.ru>
2021-11-29 01:39:24 +0300

    определение номера покуп.

--- smo/soluton/smo.py
+++ smo/soluton/smo.py
@@ -1,5 +1,6 @@
 import allocation as al
 import random
+from tabulate import tabulate
 import math
 PinH = 10 # клиенты в час
 avg_time_PinH = 60/PinH # среднее время
@@ -10,23 +11,22 @@ if __name__ == '__main__':
 #################################################################################################
 # список покупателей
     list_buers = list()
-    for i in range(size):
+    for i in range(1, size+1):
         list_buers.append(i)
 #################################################################################################
     random.seed(12045)
     duration_of_service = al.exponential_distribution(scale=avg_min, nums=size, print_graph=False) # время обслуживания
     clients = al.exponential_distribution(scale=avg_time_PinH, nums=size, print_graph=False) # время поступления клиента
 
-    print("ожидание обслуживания = \n", duration_of_service)
-    print("\nвремя прихода клиентов = \n", clients)
+    # print("ожидание обслуживания = \n", duration_of_service)
+    # print("\nвремя прихода клиентов = \n", clients)
 #################################################################################################
 # t пос кумулятивное
     t_kumulitive_list = list() # время комулетивное
-    sum = 0
-    for t_ob in clients:
-        sum +=t_ob
-        t_kumulitive_list.append(round(sum, 2))
-    print("время поступления комулятивное = \n", t_kumulitive_list)
+    t_kumulitive_list.append(round(clients[0], 2))
+    for i in range(1, size):
+        t_kumulitive_list.append(round(clients[i]+t_kumulitive_list[i-1], 2))
+    # print("время поступления комулятивное = \n", t_kumulitive_list)
 #################################################################################################
 # время окончания обслуживания
 #
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         t_serv = duration_of_service[i]
         #print(t_kum, "  ", t_serv, "Time")
         t_end_service.append(round((t_kum+t_serv), 2))
-    print("Время окончания обслуживания = \n", t_end_service)
+    # print("Время окончания обслуживания = \n", t_end_service)
 #################################################################################################
 # t начало обслуживания, осслуживание начинается после завершение предидущих, первый ослуживается сразу(время прихода)
 # если время
@@ -47,20 +47,36 @@ if __name__ == '__main__':
             t_start_service.append(t_kumulitive_list[i])
         else:
             t_start_service.append(t_end_service[i-1])
-    print("Время начало обслуживания = \n", t_start_service )
+        if(t_end_service[i - 1] + duration_of_service[i] > t_end_service[i]):
+            t_end_service[i] = t_end_service[i - 1] + duration_of_service[i]
+    # print("Время начало обслуживания = \n", t_start_service )
 #################################################################################################
 # Время в системе разница между временим поступления и обслуживанием
 #
     system_in_time = list()
     for i in range(size):
         system_in_time.append(round(t_end_service[i] - t_kumulitive_list[i], 2))
-    print("Время в системе = \n", system_in_time)
+    # print("Время в системе = \n", system_in_time)
 #################################################################################################
 # время в очереди
     time_in_queue = list()
     for i in range(size):
-        time_in_queue.append(round(system_in_time[i] - duration_of_service[i], 3)) # ошибка
-print("Время в очереди = \n", time_in_queue)
+        time_in_queue.append(round(t_start_service[i] - t_kumulitive_list[i], 3)) # ошибка
+    # print("Время в очереди = \n", time_in_queue)
 #################################################################################################
 # определяем номер предидущего обслуженного покупателя в момент прихода следующего
-#    number_buer_
+    num_obs = list()
+    for i in list_buers:
+        if(t_kumulitive_list[i]==t_start_service[i]):
+            num_obs.append(i)
+        
+
+    all_stat = dict()
+
+    for i in range(size):
+        all_stat.update({list_buers[i]: (round(clients[i],2) ,round(duration_of_service[i],2), t_kumulitive_list[i],t_start_service[i],
+                                     t_end_service[i],system_in_time[i],time_in_queue[i] )})
+    headers = ['№ клиента','Время поступ','Время обслуживания','Т кумулятив','Начало обслуживания','конец обслуживания',
+               'время в сист','время в очереди']
+
+    print(tabulate([(k,) + v for k,v in all_stat.items()], headers=headers))
