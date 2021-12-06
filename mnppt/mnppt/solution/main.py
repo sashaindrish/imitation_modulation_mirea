1e446321bbd556474f334fe334ebccc0dc0feecd
Alexandr <31647857+sashaindrish@users.noreply.github.com>
2021-12-06 11:22:51 +0300

    ip блоки

--- mnppt/mnppt/solution/main.py
+++ mnppt/mnppt/solution/main.py
@@ -3,19 +3,19 @@ import math
 
 from tabulate import tabulate
 
-def formula_a(t):  # время в часах
+def formula_a(t):  # время 
     return 15 + 10 * math.sin((3 * math.pi * t) / 24)
 
 
-def formula_b(t):  # время в часах
+def formula_b(t):  # время
     return 40 + 30 * math.sin(((4 * math.pi * t) / 24) - math.pi / 2)
 
 
-def formula_c(t):  # время в часах
+def formula_c(t):  # время 
     return 100 - 50 * abs((6 - math.fmod(t, 12)) / 6)
 
 
-def formula_d(t):  # время в часах
+def formula_d(t):  # время 
     if t >= 6:
         return 15 + 30 * (math.fmod(t, 12))
     else:
@@ -26,9 +26,9 @@ hour_modulation = 24
 round_n = 2
 if __name__ == '__main__':
 
-    PinH = 10  # клиенты в час / начало вариант б - 20 ч/ч
-    avg_time_PinH = 60 / PinH  # среднее время интенсивности почступления заявок
-    size = 1000  # огранечение посетителей
+    PinH = 10  # запросы в 1 времени 
+    avg_time_PinH = 60 / PinH  # среднее время интенсивности поступления заявок
+    size = 1000  # огранечение запросов
     avg_min = 5  # время на обслуживание (интенсивность обслуживания)
     final_stat = dict()
 
@@ -67,23 +67,23 @@ if __name__ == '__main__':
         final_stat.update(temp_stat)
         #print(temp_stat)
 
-    headers = ['№ клиента', 'Время поступ', 'Время обслуживания', 'Т кумулятив', 'Начало обслуживания',
+    headers = ['№ запроса', 'Время поступ', 'Время обслуживания', 'Т кумулятив', 'Начало обслуживания',
                'конец обслуживания',
-               'время в сист', 'время в очереди', '№ осбл. покуп', 'Длинна очереди', 'Время простоя']
+               'время в сист', 'время в очереди', '№ осбл. заявки', 'Длинна очереди', 'Время простоя']
 
     print(tabulate([(k,) + v for k, v in final_stat.items()], headers=headers))
 
     print("Выходные данные :")
 
-    print("Cреднее время интенсивности почступления заявок (минут)= ", round(aavg_time_PinH/hour_modulation, round_n))
+    print("Cреднее время интенсивности почступления заявок (временных единиц)= ", round(aavg_time_PinH/hour_modulation, round_n))
     print("Время на обслуживание (интенсивность обслуживания) =", avg_min)
 
 
     print("\n Основные параметры системы \n")
 
-    print("Время потраченное на обслуживание(минут) =",t_kumulitive_end)
-    print("Коэфициент загружености продавца = ", round(KZP/hour_modulation, round_n))
-    print("Средняя длинна очереди (человек) = ", round(SDO/hour_modulation, round_n))
-    print("Среднее время ожидания в очереди (минут) = ", round(SVOvO/hour_modulation, round_n))
-    print("Максимальная длинна в очереди (человек) = ", max(MaxLQ))
-    print("Среднее время пребывания заявок в системе (минут) = ", round(SVPZvS/hour_modulation, round_n))
+    print("Время потраченное на обслуживание(временных единиц) =",t_kumulitive_end)
+    print("Коэфициент загружености IP-блока = ", round(KZP/hour_modulation, round_n))
+    print("Средняя длинна очереди (запросов) = ", round(SDO/hour_modulation, round_n))
+    print("Среднее время ожидания в очереди (временных единиц) = ", round(SVOvO/hour_modulation, round_n))
+    print("Максимальная длинна в очереди (запросов) = ", max(MaxLQ))
+    print("Среднее время пребывания заявок в системе (временных единиц) = ", round(SVPZvS/hour_modulation, round_n))
